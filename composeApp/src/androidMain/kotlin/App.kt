8d7e21234d641469526348fa2697e65cf79813ab
Busola-ATL <agorobusola3@gmail.com>
2024-06-14 09:41:25 -0400

    CalcEqualsButton button holding a padding of 4 dp, and a text field wit the value "=". The onClick sets the display value to "0".

--- composeApp/src/androidMain/kotlin/App.kt
+++ composeApp/src/androidMain/kotlin/App.kt
@@ -2,12 +2,14 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -57,5 +59,11 @@ fun CalcOperationButton(){
 }
 @Composable
 fun CalcEqualsButton(display: MutableState<String>){
-
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier
+            .padding(4.dp)
+    ) {
+        Text(text = "=")
+    }
 }
